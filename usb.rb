5a95f265c9377d81f65057824352b9e02268bc40
550502daryachyruk <31868315+550502daryachyruk@users.noreply.github.com>
2017-12-16 10:17:08 +0300

    Changed again

--- usb.rb
+++ usb.rb
@@ -6,9 +6,12 @@ Shoes.app title: 'USB', width: 530, height: 400 do
   @eject_button = button 'Eject device', margin: 15, width: 40
   @button = button 'Device list', margin: 15, margin_left: 220
   @edit_box = edit_box margin: 20, width: 480, height: 150
+
   @button.click do
+
     Thread.new do
       loop do
+
         @other = `lsusb -v`
         @other.split("\n").each do |item|
           if item.include?('iProduct') and item.split(' ').size == 5
@@ -16,25 +19,28 @@ Shoes.app title: 'USB', width: 530, height: 400 do
           end
         end
         memory = `df -h`
-        media = `ls /media/dasha`
+        media = `ls /media/*`
         media.split("\n").each do |device|
           memory.split("\n").each do |memory|
             if memory.include?device
               @edit_box.text += device + "\n"
-              @edit_box.text += "Size: #{memory.split(' ')[1]}\n"
-              @edit_box.text += "Used: #{memory.split(' ')[2]}\n\n"
+              @edit_box.text += "Size is: #{memory.split(' ')[1]}\n"
+              @edit_box.text += "Used memory: #{memory.split(' ')[2]}\n\n"
             end
           end
         end
+
         sleep 5
       end
     end
+
   end
+
   @eject_button.click do
     memory = `df -h`
     memory.split("\n").each do |memory|
       if memory.include?@line.text
-        `sudo umount -f #{memory.split(' ')[0]}`
+        `umount #{memory.split(' ')[0]}`
       end
     end
   end
